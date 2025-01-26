e3605b0583f449a4038e000df7e2ed183ef2e3c6
Naseeb Ahmed <naseeb.devpro@gmail.com>
2025-01-26 22:34:03 +0500

    --amend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
     // baseURL: import.meta.env.MODE === "development" ? 'http://localhost:3000/api' : "/api",
-    baseURL: import.meta.env.BASE_URL + "/api",
+    baseURL: "https://connectly-chat-backend.vercel.app//api",
     withCredentials: true,
 })
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,6 +4,7 @@ import toast from "react-hot-toast";
 import { useChatStore } from "./useChatStore";
 import { io } from 'socket.io-client';
 
+const BASE_URL = "https://connectly-chat-backend.vercel.app"
 // const BASE_URL = import.meta.env.MODE === "development" ? 'http://localhost:3000' : "/";
 
 export const useAuthStore = create((set, get) => ({
@@ -89,7 +90,7 @@ export const useAuthStore = create((set, get) => ({
         const { authUser } = get();
         if(!authUser || get().socket?.connected) return;
 
-        const socket = io(import.meta.env.BASE_URL, {
+        const socket = io(BASE_URL, {
             query: {
                 userId: authUser._id,
             },
