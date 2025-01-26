3f763aec4c74c192b22302000b6fb15a0e8ab118
Naseeb Ahmed <naseeb.devpro@gmail.com>
2025-01-26 22:54:55 +0500

    make changes for env variables

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,13 +1,15 @@
 import { Server } from 'socket.io';
 import http from 'http';
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
 const server = http.createServer(app);
+dotenv.config();
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"]
+        origin: [process.env.CLIENT_URL],
     },
 });
 
