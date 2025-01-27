a7ff38bffb6970d7471341bc37997eca8c39c7d7
Abubakar <abubakkeramjad10@gmail.com>
2025-01-27 22:38:00 +0500

    fixed vercel.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,33 @@
-import express from 'express';
-import { connectDB } from './lib/db.js';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import { app, server } from './lib/socket.js';
+import express from "express";
+import { connectDB } from "./lib/db.js";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const origin = process.env.NODE_ENV === 'production'
+  ? 'https://connectly-chat-frontend.vercel.app'
+  : 'http://localhost:5173';
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+
+app.use(
+  cors({
+    origin,
     credentials: true,
-}));
+  })
+);
 
 app.use("/auth", authRoutes);
 app.use("/message", messageRoutes);
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
-})
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
